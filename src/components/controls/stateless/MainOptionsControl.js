269fea38333429a87ae51f4d8eedcd6db1e4fab8
Markus Mörsky <markus.morsky@taigarevolution.fi>
2017-11-07 22:05:27 +0200

    + tuning to main options

--- src/components/controls/stateless/MainOptionsControl.js
+++ src/components/controls/stateless/MainOptionsControl.js
@@ -2,72 +2,98 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { CardDeck,
          Card,
+         CardImgOverlay,
          CardImg,
          CardBody,
          CardTitle } from 'reactstrap';
 import {MainOptions} from '../../../common/enumerations';
 
 const cardStyle = {
- maxWidth: '30%'
+ width: '200px',
+ height: '200px',
+ padding: '10px 30px 50px 30px'
+};
+
+const overlayStyle = {
+  display: 'flex',
+  justifyContent: 'center',
+  alignItems: 'flex-end'
+};
+
+const imgStyle = {
+  maxHeight: '100%',
+  maxWidth: '100%'
 };
 
 const MainOptionsControl = ({onOptionSelected}) => {
-  let auctionsImgUrl = "";
-  let itemLocationsImgUrl = "";
-  let deliveryOptionsImgUrl = "";
-  let paymentOptionsImgUrl = "";
-  let contactInfosImgUrl = "";
-  let usersImgUrl = "";
+  let auctionsImgUrl = '../../../assets/main/auction.jpg';
+  let itemLocationsImgUrl = "../../../assets/main/locations.png";
+  let deliveryOptionsImgUrl = "../../../assets/main/delivery.png";
+  let paymentOptionsImgUrl = "../../../assets/main/payment.png";
+  let contactInfosImgUrl = "../../../assets/main/contact_info.jfif";
+  let usersImgUrl = '../../../assets/main/users.png';
 
   return (
     <CardDeck>
       <Card id="manageAuctionsCard"
-            style={cardStyle}
-            onClick={() => onOptionSelected(MainOptions.AUCTIONS)}>
-        <CardImg top width="100%" src={auctionsImgUrl} alt="image" />
-        <CardBody>
-          <CardTitle>Auctions</CardTitle>
-        </CardBody>
+            onClick={() => onOptionSelected(MainOptions.AUCTIONS)}
+            style={cardStyle}>
+        <CardImg style={imgStyle} src={auctionsImgUrl} alt="image" />
+        <CardImgOverlay style={overlayStyle}>
+          <div style={{backgroundColor: 'black', padding: '10px', opacity: '0.6', borderRadius: '15px'}}>
+            <h4 style={{color: 'white'}}>Auctions</h4>
+          </div>
+        </CardImgOverlay>
       </Card>
       <Card id="manageItemLocationsCard"
             style={cardStyle}
             onClick={() => onOptionSelected(MainOptions.ITEM_LOCATIONS)}>
-        <CardImg top width="100%" src={itemLocationsImgUrl} alt="image" />
-        <CardBody>
-          <CardTitle>Item locations</CardTitle>
-        </CardBody>
+        <CardImg style={imgStyle} src={itemLocationsImgUrl} alt="image" />
+        <CardImgOverlay style={overlayStyle}>
+          <div style={{backgroundColor: 'black', padding: '10px', opacity: '0.6', borderRadius: '15px'}}>
+            <h4 style={{color: 'white'}}>Locations</h4>
+          </div>
+        </CardImgOverlay>
       </Card>
       <Card id="manageDeliveryOptionsCard"
             style={cardStyle}
             onClick={() => onOptionSelected(MainOptions.DELIVERY_OPTIONS)}>
-        <CardImg top width="100%" src={deliveryOptionsImgUrl} alt="image" />
-        <CardBody>
-          <CardTitle>Delivery options</CardTitle>
-        </CardBody>
+        <CardImg style={imgStyle} src={deliveryOptionsImgUrl} alt="image" />
+        <CardImgOverlay style={overlayStyle}>
+          <div style={{backgroundColor: 'black', padding: '10px', opacity: '0.6', borderRadius: '15px'}}>
+            <h4 style={{color: 'white'}}>Delivery</h4>
+          </div>
+        </CardImgOverlay>
       </Card>
       <Card id="managePaymentOptionsCard"
             style={cardStyle}
             onClick={() => onOptionSelected(MainOptions.PAYMENT_OPTIONS)}>
-        <CardImg top width="100%" src={paymentOptionsImgUrl} alt="image" />
-        <CardBody>
-          <CardTitle>Payment options</CardTitle>
-        </CardBody>
+        <CardImg style={imgStyle} src={paymentOptionsImgUrl} alt="image" />
+        <CardImgOverlay style={overlayStyle}>
+          <div style={{backgroundColor: 'black', padding: '10px', opacity: '0.6', borderRadius: '15px'}}>
+            <h4 style={{color: 'white'}}>Payments</h4>
+          </div>
+        </CardImgOverlay>
       </Card>
       <Card id="manageContactInfosCard"
             style={cardStyle}
             onClick={() => onOptionSelected(MainOptions.CONTACT_INFOS)}>
-        <CardImg top width="100%" src={contactInfosImgUrl} alt="image" />
-        <CardBody>
-          <CardTitle>Contact infos</CardTitle>
-        </CardBody>
+        <CardImg style={imgStyle} src={contactInfosImgUrl} alt="image" />
+        <CardImgOverlay style={overlayStyle}>
+          <div style={{backgroundColor: 'black', padding: '10px', opacity: '0.6', borderRadius: '15px'}}>
+            <h4 style={{color: 'white'}}>Contact</h4>
+          </div>
+        </CardImgOverlay>
       </Card>
       <Card id="manageUsersCard"
             style={cardStyle}
             onClick={() => onOptionSelected(MainOptions.USERS)}>
-        <CardImg top width="100%" src={usersImgUrl} alt="image" />
-        <CardBody>
-          <CardTitle>Users</CardTitle>
-        </CardBody>
+        <CardImg style={imgStyle} src={usersImgUrl} alt="image" />
+        <CardImgOverlay style={overlayStyle}>
+          <div style={{backgroundColor: 'black', padding: '10px', opacity: '0.6', borderRadius: '15px'}}>
+            <h4 style={{color: 'white'}}>Users</h4>
+          </div>
+        </CardImgOverlay>
       </Card>
     </CardDeck>
   );
